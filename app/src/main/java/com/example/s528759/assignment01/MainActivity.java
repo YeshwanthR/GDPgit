69a11940c6cc6ef24a5542b45733ef871d65e7c4
Rangineni <S528759@nwmissouri.edu>
2017-09-19 21:24:00 -0500

    GDP commit

--- app/src/main/java/com/example/s528759/assignment01/MainActivity.java
+++ app/src/main/java/com/example/s528759/assignment01/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+// This is a GDP commit
 
     public void buttonAction(View view)
     {
